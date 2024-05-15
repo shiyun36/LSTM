dbac6064c19527bf300f35c757d51d5d5ada5b47
shiyun36 <111891177+shiyun36@users.noreply.github.com>
2024-05-15 09:56:29 +0200

    Update and rename NYTimes Comments to dataset.py

---
+++ dataset.py
@@ -0,0 +1,32 @@
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Dropout
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+import keras.utils as ku 
+
+from tensorflow import set_random_seed
+from numpy.random import seed
+set_random_seed(2)
+seed(1)
+
+import pandas as pd
+import numpy as np
+import string, os 
+
+import warnings
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+curr_dir = '../NewYorkTimes/'
+all_headlines = []
+for filename in os.listdir(curr_dir):
+    if 'Articles' in filename:
+        article_df = pd.read_csv(curr_dir + filename)
+        all_headlines.extend(list(article_df.headline.values))
+        break
+
+all_headlines = [h for h in all_headlines if h != "Unknown"]
+len(all_headlines)
+
+
